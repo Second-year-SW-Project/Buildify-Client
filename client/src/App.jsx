763d3092c878e06e388377fbfe08eb7651c77d7d
Lasitha Hasaranga <lasithamuthukumarana2022@gmail.com>
2025-03-16 12:02:36 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,173 @@
-import "./App.css";
-import { useState } from "react";
-import Navbar from "./MoleculesComponents/User_component/Navbar";
-import Footer from "./MoleculesComponents/User_component/Footer";
-import Test from "./pages/Test";
-import Home from "./pages/Home/Home";
-import Productcategorypage from "./pages/Producat_category/Productcategorypage";
-import Itempage from "./pages/Single_Item/Itempage";
+import { Outlet } from "react-router-dom";
+import { AppProvider } from "@toolpad/core/react-router-dom";
+import Iconset from './AtomicComponents/Icons/Iconset';
+import theme from './AtomicComponents/theme';
+
+const NAVIGATION = [
+  {
+    kind: 'header',
+    title: 'Main',
+  },
+  {
+    title: 'Dashboard',
+    icon: <Iconset type="dashboard" />,
+  },
+  {
+    segment: 'admin',
+    title: 'Admin',
+    icon: <Iconset type="admin" />,
+    children: [
+      {
+        segment: 'profile',
+        title: 'Profile',
+        icon: <Iconset type="profile" />,
+      },
+      {
+        segment: 'setting',
+        title: 'Settings',
+
+        icon: <Iconset type='settings' />,
+      },
+    ],
+  },
+  {
+    kind: 'divider',
+  },
+  {
+    kind: 'header',
+    title: 'Product & Order',
+  },
+  {
+    segment: 'products',
+    title: 'Products',
+    icon: <Iconset type="product" />,
+    children: [
+      {
+        segment: 'manageproduct',
+        title: 'Manage Product',
+        icon: <Iconset type="productList" />,
+      },
+      {
+        segment: 'createproduct',
+        title: 'Create Product',
+        icon: <Iconset type="productCreate" />,
+      },
+    ],
+
+  },
+  {
+    segment: 'orders',
+    title: 'Orders ',
+    icon: <Iconset type="order" />,
+    children: [
+      {
+        segment: 'orderlist',
+        title: 'Order List',
+        icon: <Iconset type="orderList" />,
+      },
+      {
+        segment: 'receiveorder',
+        title: 'Received Order',
+        icon: <Iconset type="receiveOrder" />,
+      },
+    ],
+  },
+  {
+    kind: 'divider',
+  },
+  {
+    kind: 'header',
+    title: 'User Section',
+  },
+  {
+    segment: 'usermanage',
+    title: 'User Manage',
+    icon: <Iconset type="user-manage" />,
+
+  },
+  {
+    kind: 'divider',
+  },
+  {
+    kind: 'header',
+    title: 'Feedbacks',
+  },
+  {
+    segment: 'feedbackmanage',
+    title: 'Feedback Manage',
+    icon: <Iconset type="feedback-manage" />,
+    children: [
+      {
+        segment: 'complaints',
+        title: 'Complains',
+        icon: <Iconset type="complains" />,
+      },
+      {
+        segment: 'comments&reviews',
+        title: 'Comments & Reviews',
+        icon: <Iconset type="comments" />,
+      },
+      {
+        segment: 'rma',
+        title: 'RMA',
+        icon: <Iconset type="RMA" />,
+      }
+    ],
+  },
+  {
+    kind: 'divider',
+  },
+  {
+    kind: 'header',
+    title: 'Invoices',
+  },
+  {
+    segment: 'invoice',
+    title: 'Invoice ',
+    icon: <Iconset type="invoice" />,
+    children: [
+      {
+        segment: 'invoicelist',
+        title: 'Invoice List',
+        icon: <Iconset type="invoiceList" />,
+      },
+      {
+        segment: 'invoicecreate',
+        title: 'Create Invoice',
+        icon: <Iconset type="createInvoice" />,
+      },
+    ],
+  },
+
+];
 
 function App() {
+
+
   return (
-    <div>
-      <Home></Home>
-    </div>
-  );
+    <AppProvider
+      navigation={NAVIGATION}
+      branding={{
+        logo: (<img
+          src='/src/assets/images/Logos/logo-white.png'
+          alt='Logo'
+          style={{
+            marginLeft: '8px',
+            marginTop: '4px',
+            maxWidth: '100%',
+            height: 'auto',
+            width: '140px'
+          }}
+        />
+        ),
+        title: '',
+      }}
+      theme={theme}
+    >
+      <Outlet />
+    </AppProvider>
+  )
+
 }
 
 export default App;
