23c184e24857476ffaaa2afcee1ab41ef38c08b7
Gethmi-Rathnayaka <gethmisrathnayaka@gmail.com>
2025-03-20 00:16:27 +0530

    tests and path changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import Home_part1 from './Home/Home_part1'
-import Home_part2 from './Home/Home_part2/Home_part2'
-import Home_part4 from './Home/Home_part4/Home_part4'
-import Navbar from '../MoleculesComponents/User_navbar_and_footer/Navbar'
-import Footer from '../MoleculesComponents/User_navbar_and_footer/Footer'
+import React from "react";
+import Home_part1 from "./Home/Home_part1";
+import Home_part2 from "./Home/Home_part2/Home_part2";
+import Home_part4 from "./Home/Home_part4/Home_part4";
+import Navbar from "../MoleculesComponents/User_component/Navbar";
+import Footer from "../MoleculesComponents/User_component/Footer";
 
 export default function Home() {
   return (
     <div>
-        <Navbar></Navbar>
-        <Home_part1></Home_part1>
-        
-        <Home_part2></Home_part2>
-        <Home_part4></Home_part4>
-        <Footer></Footer>
+      <Navbar></Navbar>
+      <Home_part1></Home_part1>
 
+      <Home_part2></Home_part2>
+      <Home_part4></Home_part4>
+      <Footer></Footer>
     </div>
-  )
+  );
 }
