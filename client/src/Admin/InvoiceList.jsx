f9a30bbcf58d7f580ac4339af48c68ccb355aca3
Gethmi-Rathnayaka <gethmisrathnayaka@gmail.com>
2025-04-16 00:01:57 +0530

    invoicelist

--- client/src/Admin/InvoiceList.jsx
+++ client/src/Admin/InvoiceList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { UserTable } from "../MoleculesComponents/Table";
 import CustomerCard from "../AtomicComponents/Cards/Customercard";
-import TimeCard from "../AtomicComponents/Cards/TimeCard";
+import DateCard from "../AtomicComponents/Cards/Datecard";
 import CustomBreadcrumbs from "../AtomicComponents/Breadcrumb";
 import { PageTitle } from "../AtomicComponents/Typographics/TextStyles";
 import { AddButton } from "../AtomicComponents/Buttons/Buttons";
@@ -19,7 +19,6 @@ function InvoiceList() {
     { id: "startdate", label: "Created" },
     { id: "enddate", label: "Due" },
     { id: "amount", label: "Amount" },
-    { id: "status", label: "Status" },
   ];
 
   const invoiceData = [
@@ -27,71 +26,74 @@ function InvoiceList() {
       customerCard: (
         <CustomerCard
           name="Gethmi Rathnayaka"
-          invId="INV1243"
+          type="INV1234"
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi50-FcEbEamVMqvkhUo8jklEge0uZIG9pbg&s"
         />
       ),
-      startdate: <TimeCard date="2024-02-14" time="1.30 pm" />,
-      enddate: <TimeCard date="2024-02-14" time="1.30 pm" />,
+      startdate: <DateCard date="2024-02-14" />,
+      enddate: <DateCard date="2024-02-14" />,
       amount: "720,000 LKR",
-      status: "Inactive",
     },
   ];
 
   const iconTypes = ["view", "edit", "delete"];
 
   return (
-    <div className="pl-6 grid grid-rows">
-      <div className="mt-3">
-        <PageTitle value="Invoice List"></PageTitle>
-        <CustomBreadcrumbs
-          paths={[
-            { label: "Invoice", href: "/invoice/invoicelist" },
-            { label: "Invoice List" },
-          ]}
-        />
-      </div>
+    <div>
+      <div className="ml-[330px] -mt-[600px]">
+        <div className="pl-6 grid grid-rows">
+          <div className="mt-3">
+            <PageTitle value="Invoice List"></PageTitle>
+            <CustomBreadcrumbs
+              paths={[
+                { label: "Invoice", href: "/invoice/invoicelist" },
+                { label: "Invoice List" },
+              ]}
+            />
+          </div>
 
-      <div className="pb-4 mr-4">
-        <div className="float-right">
-          <AddButton
-            name="Create Invoice"
-            isBold={1}
-            buttonSize="medium"
-            fontSize="16px"
-            onClick={() => navigate("/invoice/invoicecreate")}
-          />
-        </div>
-      </div>
-
-      <div className="mr-4 border-2 border-black-200 rounded-md">
-        <div className="filterForm grid gap-4 grid-cols-1 flex flex-row p-4">
-          <div className="filterFormProperty1 grid gap-y-4 gap-x-4 grid-cols-4 flex flex-row">
-            <div>
-              <InputField
-                type="select"
-                label="All Orders"
-                options={StockType}
-                width="100%"
+          <div className="pb-4 mr-4">
+            <div className="float-right">
+              <AddButton
+                name="Create Invoice"
+                isBold={1}
+                buttonSize="medium"
+                fontSize="16px"
+                onClick={() => navigate("/invoice/invoicecreate")}
               />
             </div>
-            <div>
-              <SetDate width="100%" label="Start Date"></SetDate>
+          </div>
+
+          <div className="mr-4 border-2 border-black-200 rounded-md">
+            <div className="filterForm grid gap-4 grid-cols-1 flex flex-row p-4">
+              <div className="filterFormProperty1 grid gap-y-4 gap-x-4 grid-cols-4 flex flex-row">
+                <div>
+                  <InputField
+                    type="select"
+                    label="All Orders"
+                    options={StockType}
+                    width="100%"
+                  />
+                </div>
+                <div>
+                  <SetDate width="100%" label="Start Date"></SetDate>
+                </div>
+                <div>
+                  <SetDate width="100%" label="End Date"></SetDate>
+                </div>
+                <div>
+                  <SearchBar placeholder="Search" width="100%"></SearchBar>
+                </div>
+              </div>
             </div>
-            <div>
-              <SetDate width="100%" label="End Date"></SetDate>
-            </div>
-            <div>
-              <SearchBar placeholder="Search" width="100%"></SearchBar>
+            <div sx={{ width: "100%", borderRadius: "20px" }}>
+              <UserTable
+                columns={invoiceColumns}
+                data={invoiceData}
+                iconTypes={iconTypes}
+              />
             </div>
           </div>
-        </div>
-        <div sx={{ width: "100%", borderRadius: "20px" }}>
-          <UserTable
-            columns={invoiceColumns}
-            data={invoiceData}
-            iconTypes={iconTypes}
-          />
         </div>
       </div>
     </div>
