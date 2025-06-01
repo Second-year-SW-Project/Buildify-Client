b501753d5c4e1f9e542d6f8a6f63ea12ebfac55e
Lasitha2001 <lasithamuthukumarana2022@gmail.com>
2025-06-01 20:49:21 +0530

    update: Add GameCard and TopProductCard with additional imports

--- client/src/AtomicComponents/Cards/Productcard.jsx
+++ client/src/AtomicComponents/Cards/Productcard.jsx
@@ -1,8 +1,8 @@
-import React from "react";
 import { Typography } from "@mui/material";
+import Iconset from "../Icons/Iconset";
+import theme from "../theme";
 
-
-export default function ProductCard({ name, type, src, id }) {
+export function ProductCard({ name, type, src, id }) {
     return (
         <div className="flex items-center space-x-3">
             <div>
@@ -14,6 +14,44 @@ export default function ProductCard({ name, type, src, id }) {
                     whiteSpace: "normal", // Enable wrapping
                 }}>{name}</Typography>
                 <Typography variant="body2" fontWeight="bold" color="primary">{type}</Typography>
+            </div>
+        </div>
+    );
+}
+
+export function GameCard({ name, type, src }) {
+    return (
+        <div className="flex items-center space-x-3">
+            <div>
+                <img className="max-w-14 max-h-14" src={src} alt="image" />
+            </div>
+            <div>
+                <Typography variant="body2" fontWeight="bold" style={{
+                    wordBreak: "break-word", // Allow breaking long words
+                    whiteSpace: "normal", // Enable wrapping
+                }}>{name}</Typography>
+            </div>
+        </div>
+    );
+}
+
+export function TopProductCard({ name, orderCount, src, sales, rate }) {
+    return (
+        <div className="flex flex-row items-center">
+            <div className="flex justify-center items-center">
+                <img className="max-w-16 max-h-16" src={src} alt="image" />
+            </div>
+            <div className="flex flex-col">
+                <Typography variant="body2" fontWeight="bold" style={{
+                    wordBreak: "break-word", // Allow breaking long words
+                    whiteSpace: "normal", // Enable wrapping
+                }}>{name}</Typography>
+                <div className="flex flex-row items-center space-x-1">
+                    <Iconset type="attachMoney" fontSize="30px" color="primary"></Iconset>
+                    <Typography variant="caption" color="ternary">Sales: {sales} LKR</Typography>
+                    <Iconset type="star" fontSize="30px" color="warning"></Iconset>
+                    <Typography variant="caption" fontWeight="bold" >{rate}</Typography>
+                </div>
             </div>
         </div>
     );
